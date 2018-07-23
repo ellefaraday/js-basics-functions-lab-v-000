9349fb6ffbd8d99c1a6430d818c841ff0953a042
Elle Faraday <faraday.elle@gmail.com>
2018-07-23 21:24:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  const distance = Math.abs(42 - location);
+  return distance;
+}
+
+function distanceFromHqInFeet(location) {
+  const feet = distanceFromHqInBlocks(location)*264;
+  return feet;
+}
+
+function distanceTravelledInFeet(start, end) {
+  const distance = Math.abs(start - end) * 264
+  return distance;
+}
+
+function calculatesFarePrice(start, destination) {
+  let price;
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance > 2500) {
+    console.log();
+  }
+}
